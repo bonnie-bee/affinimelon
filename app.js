715e736cb76f854dec09a44e0165a9a9f6165c18
Bonnie <birchba@gmail.com>
2021-04-07 20:25:29 -0500

    updating port for heroku

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 8080;
 
 
 
-app.listen(PORT, function () {
-    console.log(`Server listening on: http://localhost:${PORT}`)
+app.listen(process.env.PORT || 8080, function () {
+    console.log(`Server listening on: http://localhost:8080`)
 })
 
 app.use(express.static(path.join(__dirname, 'public')));
